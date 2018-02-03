d1c85aa6d07c22920f0aabfb88d6ec508d4a9a6b
Rebecca Wagaman <rebecca.wagaman@gmail.com>
2018-02-03 17:57:31 -0500

    Add NUnit annotations, passed test

--- SanbanScraper/SanbanScraper/NUnitTest.cs
+++ SanbanScraper/SanbanScraper/NUnitTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
@@ -10,19 +11,21 @@ namespace SanbanScraper
 {
     class NUnitTest
     {
-
         IWebDriver driver;
 
+        [SetUp]
         public void Initialize()
         {
             driver = new ChromeDriver();
         }
 
+        [Test]
         public void OpenAppTest()
         {
             driver.Url = "https://finance.yahoo.com";
         }
 
+        [TearDown]
         public void EndTest()
         {
             driver.Close();
