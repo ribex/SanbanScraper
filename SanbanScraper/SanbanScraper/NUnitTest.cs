76c8881ae71f2b73d24c27445def800fdc194186
Rebecca Wagaman <rebecca.wagaman@gmail.com>
2018-02-03 17:53:23 -0500

    Create initial test

---
+++ SanbanScraper/SanbanScraper/NUnitTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SanbanScraper
+{
+    class NUnitTest
+    {
+
+        IWebDriver driver;
+
+        public void Initialize()
+        {
+            driver = new ChromeDriver();
+        }
+
+        public void OpenAppTest()
+        {
+            driver.Url = "https://finance.yahoo.com";
+        }
+
+        public void EndTest()
+        {
+            driver.Close();
+        }
+    }
+}
